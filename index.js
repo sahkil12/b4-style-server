7c369a72dd6092eff92930bfe18971d2e846ed60
Tazwer Shakil <tazwershakilshakil@gmail.com>
2026-01-29 18:53:30 +0600

    connect mongodb with server successfully

--- index.js
+++ index.js
@@ -1,61 +1,37 @@
 const express = require("express");
 const cors = require("cors");
-
+require("dotenv").config();
 const app = express();
 const port = 5000;
-
+const { MongoClient } = require('mongodb');
 // middleware
 app.use(cors());
 app.use(express.json());
 
-// 🔹 Demo products (later MongoDB দিয়ে replace করবে)
-const products = [
-  {
-    id: 1,
-    title: "Oversized T-Shirt",
-    category: "T-SHIRTS",
-    price: 850,
-    image: "https://i.ibb.co/example1.jpg"
-  },
-  {
-    id: 2,
-    title: "Black Hoodie",
-    category: "HOODIES",
-    price: 1650,
-    image: "https://i.ibb.co/example2.jpg"
-  },
-  {
-    id: 3,
-    title: "Drop Shoulder Tee",
-    category: "T-SHIRTS",
-    price: 950,
-    image: "https://i.ibb.co/example3.jpg"
-  }
-];
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gr8kgxz.mongodb.net/?appName=Cluster0`;
 
-// 🔹 Root test
+const client = new MongoClient(uri);
+
+async function run() {
+     try {
+          await client.connect();
+
+          await client.db("admin").command({ ping: 1 });
+          console.log("Pinged your deployment. You successfully connected to MongoDB!");
+     } finally {
+          // await client.close();
+     }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("B4 Style Backend is running 🚀");
+     res.send("B4 Style Backend is running 🚀");
 });
 
-// 🔹 Get all products
 app.get("/api/products", (req, res) => {
-  res.json(products);
+     res.json(products);
 });
 
-// 🔹 Get single product by id
-app.get("/api/products/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const product = products.find(p => p.id === id);
-
-  if (!product) {
-    return res.status(404).json({ message: "Product not found" });
-  }
-
-  res.json(product);
-});
-
-// 🔹 Start server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+     console.log(`Server running on ${port}`);
 });
