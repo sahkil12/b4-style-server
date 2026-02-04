1f5fc424f06ff35a58a1339800b46e3a2858cced
Tazwer Shakil <tazwershakilshakil@gmail.com>
2026-02-04 16:17:38 +0600

    add products data with product id route

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -15,17 +15,16 @@ const client = new MongoClient(uri);
 async function run() {
      try {
           await client.connect();
-
           const db = client.db("B4_Style");
           const productsCollection = db.collection("products");
 
           app.get("/", async (req, res) => {
                res.send("B4 Style Backend is running 🚀");
           });
-
+          // get products 
           app.get("/products", async (req, res) => {
                const query = {};
-               
+
                if (req.query.isNew) {
                     query.isNew = req.query.isNew === "true"
                }
@@ -38,6 +37,14 @@ async function run() {
                const products = await productsCollection.find(query).toArray();
                res.send(products);
           });
+          //get product with id
+          app.get("/products/:id", async (req, res) => {
+               const id = req.params.id
+               const query = { _id: new ObjectId(id) }
+               const product = await productsCollection.findOne(query)
+
+               res.send(product)
+          })
           // ping test
           await client.db("admin").command({ ping: 1 });
           console.log("Ping success 🚀");
